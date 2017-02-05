b410ace8747701fc2781eb2af90365706204cbc1
andrgolubev <andpgolubev@gmail.com>
2017-02-05 18:59:18 +0300

    Updated clear utility

--- utilities/clear.hpp
+++ utilities/clear.hpp
@@ -1,25 +1,25 @@
-#ifndef _CORRECT_CLEAR_H_
-#define _CORRECT_CLEAR_H_
+#ifndef _CLEAR_HPP_
+#define _CLEAR_HPP_
 
 #include <functional>
 #include <type_traits>
+#include <cstring>
 
 /**
- * \file correct_clear.hpp
+ * \file clear.hpp
  * \brief File defines simple utility to clear type-independent object.
  *
  * The utility allows to clear type-independent object based on the POD-ness of this object.
  * If the object is POD, it is safely cleaned by default cleaner unless user-defined cleaner is provided.
  * If the object is non-POD, cleaner must be provided by the user.
+ * Note: If the object is a pointer, prior dereferencing is required in order to modify the underlying data and not the address of the pointer.
  * As the utility accepts user-defined way to work with object, cleaning is not guaranteed to be safe unless the default cleaner is used.
- * If the object is a pointer, prior dereferencing is required in order to modify the underlying data and not the address of the pointer.
- * If the object is an array, custom cleaner is required as no size is provided to the utility.
  */
 
 namespace clsc
 {
     template<typename ObjT, bool = std::is_pod<ObjT>::value>
-    struct _clear
+    struct __clear
     {
         static void execute(ObjT& o, std::function<void(ObjT&)> cleaner)
         {
@@ -28,9 +28,9 @@ namespace clsc
     };
 
     template<typename ObjT>
-    struct _clear<ObjT, true>
+    struct __clear<ObjT, true>
     {
-        static void execute(ObjT& o, std::function<void(ObjT&)> cleaner = [](ObjT& o) { o = {}; })
+        static void execute(ObjT& o, std::function<void(ObjT&)> cleaner = [](ObjT& o) { std::memset(&o, 0, sizeof(o)); })
         {
             cleaner(o);
         }
@@ -42,14 +42,14 @@ namespace clsc
     template<typename ObjT>
     static void clear(ObjT& o)
     {
-        _clear<ObjT, std::is_pod<ObjT>::value>::execute(o);
+        __clear<ObjT>::execute(o);
     }
 
     template<typename ObjT>
     static void clear(ObjT& o, std::function<void(ObjT&)> cleaner)
     {
-        _clear<ObjT, std::is_pod<ObjT>::value>::execute(o, cleaner);
+        __clear<ObjT>::execute(o, cleaner);
     }
 }
 
-#endif /* _CORRECT_CLEAR_H_ */
+#endif /* _CLEAR_HPP_ */
