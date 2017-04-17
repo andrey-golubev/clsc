3559698643f653358a63dfece9eeafddc0a2fcf8
andrgolubev <andpgolubev@gmail.com>
2017-04-18 01:40:43 +0300

    Removed include of standard library header in comparable

--- utilities/comparable.hpp
+++ utilities/comparable.hpp
@@ -1,8 +1,6 @@
 #ifndef _COMPARABLE_HPP_
 #define _COMPARABLE_HPP_
 
-#include <type_traits>
-
 /**
  * \file comparable.hpp
  * \brief File defines utility that provides functionality to compare two entities of the same user-defined type (e.g. two objects).
