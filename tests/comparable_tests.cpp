308cb616c82bfff7eae2e8483254fec4136fa9a5
andrgolubev <andpgolubev@gmail.com>
2017-04-18 00:32:55 +0300

    Updated comparable. Deleted adjustable version. Fixed some CMakeLists.

--- tests/comparable_tests.cpp
+++ tests/comparable_tests.cpp
@@ -5,33 +5,28 @@
 template<typename T>
 void compare(T&& a, T&& b)
 {
-    ASSERT_FALSE(a == b);
-    ASSERT_TRUE(a != b);
+    EXPECT_FALSE(a == b);
+    EXPECT_TRUE(a != b);
 
-    ASSERT_FALSE(a > b);
-    ASSERT_TRUE(a < b);
+    EXPECT_TRUE(a < b);
+    EXPECT_TRUE(a <= b);
 
-    ASSERT_FALSE(a >= b);
-    ASSERT_TRUE(a <= b);
+    EXPECT_FALSE(a > b);
+    EXPECT_FALSE(a >= b);
 }
 
-TEST(comparable_tests, single_inheritance)
+TEST(comparable_tests, simple_inheritance)
 {
     class A : public clsc::comparable<A>
     {
         int m_val;
     public:
-        A() : m_val(0) {}
+        A() = delete;
         A(const int& val) : m_val(val) {}
 
-        bool operator<(const A& rhs)
+        int operator()(const A& rhs)
         {
-            return m_val < rhs.m_val;
-        }
-
-        bool operator==(const A& rhs)
-        {
-            return m_val == rhs.m_val;
+            return m_val - rhs.m_val;
         }
     };
 
@@ -39,114 +34,53 @@ TEST(comparable_tests, single_inheritance)
     compare(a, b);
 }
 
-TEST(comparable_tests, multiple_inheritance)
+TEST(comparable_tests, different_base_inheritance)
 {
     class operator_mock
     {
+        int m_val;
     public:
-        bool operator<(const operator_mock&)
+        operator_mock() = delete;
+        operator_mock(int val) : m_val(val) {}
+        int operator()(const operator_mock& rhs)
         {
-            return true;
-        }
-
-        bool operator==(const operator_mock&)
-        {
-            return false;
+            auto r = m_val - rhs.m_val;
+            return m_val - rhs.m_val;
         }
     };
 
     class A : public clsc::comparable<A>, public operator_mock
     {
+    public:
+        A() = delete;
+        A(int val) : operator_mock(val) {}
     };
 
-    A a, b;
+    A a(1), b(2);
     compare(a, b);
 }
 
-TEST(comparable_tests, multiple_level_inheritance)
+TEST(comparable_tests, hierarchical_inheritance)
 {
     class operator_mock : public clsc::comparable<operator_mock>
     {
+        int m_val;
     public:
-        bool operator<(const operator_mock&)
+        operator_mock() = delete;
+        operator_mock(int val) : m_val(val) {}
+        int operator()(const operator_mock& rhs)
         {
-            return true;
-        }
-
-        bool operator==(const operator_mock&)
-        {
-            return false;
+            return m_val - rhs.m_val;
         }
     };
 
     class A : public operator_mock
     {
-    };
-
-    A a, b;
-    compare(a, b);
-}
-
-TEST(adjustable_comparable_tests, both_operators_defined)
-{
-    class A : public clsc::adjustable_comparable<A>
-    {
-        int m_val;
     public:
-        A() : m_val(0) {}
-        A(const int& val) : m_val(val) {}
-
-        bool operator<(const A& rhs)
-        {
-            return m_val < rhs.m_val;
-        }
-
-        bool operator==(const A& rhs)
-        {
-            return m_val == rhs.m_val;
-        }
+        A() = delete;
+        A(int val) : operator_mock(val) {}
     };
 
     A a(1), b(2);
     compare(a, b);
-}
-
-TEST(adjustable_comparable_tests, equal_to_operator_defined)
-{
-    class A : public clsc::adjustable_comparable<A>
-    {
-        int m_val;
-    public:
-        A() : m_val(0) {}
-        A(const int& val) : m_val(val) {}
-
-        bool operator==(const A& rhs)
-        {
-            return m_val == rhs.m_val;
-        }
-    };
-
-    A a(1), b(2);
-    ASSERT_FALSE(a == b);
-    ASSERT_TRUE(a != b);
-}
-
-TEST(adjustable_comparable_tests, less_than_operator_defined)
-{
-    class A : public clsc::adjustable_comparable<A>
-    {
-        int m_val;
-    public:
-        A() : m_val(0) {}
-        A(const int& val) : m_val(val) {}
-
-        bool operator<(const A& rhs)
-        {
-            return m_val < rhs.m_val;
-        }
-    };
-
-    A a(1), b(2);
-    ASSERT_TRUE(a < b);
-    ASSERT_FALSE(a >= b);
 }
--- utilities/comparable.hpp
+++ utilities/comparable.hpp
@@ -15,91 +15,34 @@ namespace clsc
     template<typename ContainerType>
     struct comparable
     {
+        bool operator==(const ContainerType& rhs)
+        {
+            return static_cast<bool>((*static_cast<ContainerType*>(this))(rhs) == 0);
+        }
+
         bool operator!=(const ContainerType& rhs)
         {
-            return !((*static_cast<ContainerType*>(this)) == rhs);
+            return static_cast<bool>((*static_cast<ContainerType*>(this))(rhs) != 0);
+        }
+
+        bool operator<(const ContainerType& rhs)
+        {
+            return static_cast<bool>((*static_cast<ContainerType*>(this))(rhs) < 0);
         }
 
         bool operator<=(const ContainerType& rhs)
         {
-            return ((*static_cast<ContainerType*>(this)) < rhs || (*static_cast<ContainerType*>(this)) == rhs);
+            return static_cast<bool>((*static_cast<ContainerType*>(this))(rhs) <= 0);
         }
 
         bool operator>(const ContainerType& rhs)
         {
-            return !((*static_cast<ContainerType*>(this)) <= rhs);
+            return static_cast<bool>((*static_cast<ContainerType*>(this))(rhs) > 0);
         }
 
         bool operator>=(const ContainerType& rhs)
         {
-            return !((*static_cast<ContainerType*>(this)) < rhs);
-        }
-    };
-
-
-
-    struct __has_operator_base
-    {
-    protected:
-        using yes = char;
-        struct no { char _[2]; };
-    };
-
-    template<typename Type>
-    struct __has_equal_to : public __has_operator_base
-    {
-    private:
-        template<typename ContainerType, bool (ContainerType::*)(const ContainerType&) = (&ContainerType::operator==)>
-        static yes __has_equal_to_operator(ContainerType*);
-        static no  __has_equal_to_operator(...);
-
-    public:
-        static const bool value = sizeof(__has_equal_to_operator(static_cast<Type*>(0))) == sizeof(yes);
-    };
-    template<typename T> const bool __has_equal_to<T>::value;
-
-    template<typename Type>
-    struct __has_less_than : public __has_operator_base
-    {
-    private:
-        template<typename ContainerType, bool (ContainerType::*)(const ContainerType&) = (&ContainerType::operator<)>
-        static yes __has_less_than_operator(ContainerType*);
-        static no  __has_less_than_operator(...);
-
-    public:
-        static const bool value = sizeof(__has_less_than_operator(static_cast<Type*>(0))) == sizeof(yes);
-    };
-    template<typename T> const bool __has_less_than<T>::value;
-
-    template<typename ContainerType>
-    struct adjustable_comparable
-    {
-        template<typename T = ContainerType>
-        std::enable_if_t<__has_equal_to<T>::value, bool>
-        operator!=(const T& rhs)
-        {
-            return !((*static_cast<ContainerType*>(this)) == rhs);
-        }
-
-        template<typename T = ContainerType>
-        std::enable_if_t<__has_equal_to<T>::value && __has_less_than<T>::value, bool>
-        operator<=(const T& rhs)
-        {
-            return ((*static_cast<ContainerType*>(this)) < rhs || (*static_cast<ContainerType*>(this)) == rhs);
-        }
-
-        template<typename T = ContainerType>
-        std::enable_if_t<__has_equal_to<T>::value && __has_less_than<T>::value, bool>
-        operator>(const T& rhs)
-        {
-            return !((*static_cast<ContainerType*>(this)) <= rhs);
-        }
-
-        template<typename T = ContainerType>
-        std::enable_if_t<__has_less_than<T>::value, bool>
-        operator>=(const T& rhs)
-        {
-            return !((*static_cast<ContainerType*>(this)) < rhs);
+            return static_cast<bool>((*static_cast<ContainerType*>(this))(rhs) >= 0);
         }
     };
 }
