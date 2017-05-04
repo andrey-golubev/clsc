eacbfeab0ae6099d6af000011981a76a0c462871
andrgolubev <andpgolubev@gmail.com>
2017-05-05 00:09:28 +0300

    ring_array.hpp: added push_front; refactored

--- tests/ring_array_tests.cpp
+++ tests/ring_array_tests.cpp
@@ -24,20 +24,46 @@ TEST(ring_array_test, init)
     clsc::ring_array<int, ARRAY_SIZE> buf;
 }
 
+TEST(ring_array_test, push_front)
+{
+    clsc::ring_array<int, ARRAY_SIZE> buf;
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        buf.push_front(i);
+
+    // TODO: is this a bug?
+    EXPECT_EQ(0, buf.at(0)); // push_front on first element has specific behaviour
+    for (int i = 1; i < ARRAY_SIZE; i++)
+        EXPECT_EQ(ARRAY_SIZE - i, buf.at(i));
+}
+
 TEST(ring_array_test, push_back)
 {
     clsc::ring_array<int, ARRAY_SIZE> buf;
     for (int i = 0; i < ARRAY_SIZE; i++)
         buf.push_back(i);
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        EXPECT_EQ(i, buf.at(i));
 }
 
 TEST_F(ring_array_tests, front)
+{
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        EXPECT_EQ(0, m_buf.front());
+}
+
+TEST_F(ring_array_tests, move_front)
 {
     for (int i = 0; i < ARRAY_SIZE; i++)
         EXPECT_EQ(i, m_buf.move_front());
 }
 
 TEST_F(ring_array_tests, back)
+{
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        EXPECT_EQ(ARRAY_SIZE-1, m_buf.back());
+}
+
+TEST_F(ring_array_tests, move_back)
 {
     for (int i = 0; i < ARRAY_SIZE; i++)
         EXPECT_EQ(ARRAY_SIZE-(i+1), m_buf.move_back());
@@ -47,4 +73,7 @@ TEST_F(ring_array_tests, random_access)
 {
     for (int i = 0; i < ARRAY_SIZE; i++)
         EXPECT_EQ(i, m_buf[i]);
+
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        EXPECT_EQ(i, m_buf.at(i));
 }
--- utilities/ring_array.hpp
+++ utilities/ring_array.hpp
@@ -12,13 +12,13 @@ namespace clsc
     class ring_array //TODO: think about enable_if'd comparable inheritance - add comparable version that is empty if operator() is undefined
     {
     public:
-        ring_array() : m_head(0), m_tail(0), m_capacity(N) {}
+        ring_array() noexcept(true) : m_head(0), m_tail(0), m_capacity(N), m_array() {}
 
-        ring_array(const std::initializer_list<T>& list) : m_head(0), m_tail(0), m_capacity(N), m_array(list) {}
+        ring_array(const std::initializer_list<T>& list) : m_head(0), m_tail(0), m_capacity(N), m_array(list) {} //TODO: think about removing the ctor. initializer_list is problematic
 
         size_t size() const
         {
-            return static_cast<size_t>(std::abs(m_head - m_tail));
+            return m_array.size();
         }
 
         size_t capacity() const
@@ -36,6 +36,12 @@ namespace clsc
             return m_array.data();
         }
 
+        void push_front(const T& val)
+        {
+            m_array[m_head] = val;
+            m_head = (m_head - 1) >= 0 ? (m_head - 1) % m_capacity : (m_capacity - 1);
+        }
+
         void push_back(const T& val)
         {
             if (m_tail == m_head && size() != 0)
@@ -46,36 +52,32 @@ namespace clsc
             m_tail = (m_tail + 1) % m_capacity;
         }
 
-        const T& front()
+        const T& front() const
         {
+            validate_emptiness();
             return m_array[m_head];
         }
 
         const T& move_front()
         {
-            // TODO: cannot out_of_range because of std::array?
-            if (size() == 0 && m_array.size() == 0)
-                throw std::out_of_range("array is empty");
-
+            validate_emptiness();
             const auto& val = m_array[m_head];
             m_head = (m_head + 1) % m_capacity;
             m_tail = (m_tail + 1) % m_capacity;
             return val;
         }
 
-        const T& back()
+        const T& back() const
         {
-            const auto& pos = (m_tail + m_capacity -1) % m_capacity; // TODO: check pos calculation
+            validate_emptiness();
+            const auto pos = (m_tail + m_capacity - 1) % m_capacity; // TODO: check pos calculation
             return m_array[pos];
         }
 
         const T& move_back()
         {
-            // TODO: cannot out_of_range because of std::array?
-            if (size() == 0 && m_array.size() == 0)
-                throw std::out_of_range("array is empty");
-
-            const auto& pos = (m_tail + m_capacity -1) % m_capacity; // TODO: check pos calculation
+            validate_emptiness();
+            const auto pos = (m_tail + m_capacity - 1) % m_capacity; // TODO: check pos calculation
             m_tail > 0 ? m_tail-- : m_tail = m_capacity - ((m_tail + 1) % m_capacity);
             m_head > 0 ? m_head-- : m_capacity - ((m_head + 1) % m_capacity);
             return m_array[pos];
@@ -83,19 +85,27 @@ namespace clsc
 
         T& operator[](int index)
         {
+            validate_emptiness();
             if ((index >= size() && index >= m_array.size()) || index < 0)
                 throw std::out_of_range("invalid index");
             return m_array[(m_head + index) % m_capacity];
         }
 
-        T at(int index)
+        T at(int index) const
         {
+            validate_emptiness();
             if ((index >= size() && index >= m_array.size()) || index < 0)
                 throw std::out_of_range("invalid index");
             return m_array[(m_head + index) % m_capacity];
         }
 
     private:
+        void validate_emptiness() const noexcept(false) // TODO: cannot out_of_range because of std::array?
+        {
+            if (size() == 0 && m_array.size() == 0)
+                throw std::out_of_range("array is empty");
+        }
+
         int32_t m_head, m_tail;
         const size_t m_capacity;
         std::array<T, N> m_array;
