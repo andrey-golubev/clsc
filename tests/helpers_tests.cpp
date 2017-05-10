0151675c2e8f03ee560d9206ff73e85025cc35a2
andrgolubev <andpgolubev@gmail.com>
2017-05-11 00:15:46 +0300

    helpers.hpp: added simple split method for basic_string

---
+++ tests/helpers_tests.cpp
@@ -0,0 +1,35 @@
+#include <helpers.hpp>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+TEST(helpers_tests, split_string)
+{
+    std::string str("hello world");
+    const auto array = clsc::helpers::split(str, ' ');
+    EXPECT_EQ("hello", array[0]);
+    EXPECT_EQ("world", array[1]);
+}
+
+TEST(helpers_tests, split_wstring)
+{
+    std::wstring str(L"hello world");
+    const auto array = clsc::helpers::split(str, L' ');
+    EXPECT_EQ(L"hello", array[0]);
+    EXPECT_EQ(L"world", array[1]);
+}
+
+TEST(helpers_tests, split_u16string)
+{
+    std::u16string str(u"hello world");
+    const auto array = clsc::helpers::split(str, u' ');
+    EXPECT_EQ(u"hello", array[0]);
+    EXPECT_EQ(u"world", array[1]);
+}
+
+TEST(helpers_tests, split_u32string)
+{
+    std::u32string str(U"hello world");
+    const auto array = clsc::helpers::split(str, U' ');
+    EXPECT_EQ(U"hello", array[0]);
+    EXPECT_EQ(U"world", array[1]);
+}
