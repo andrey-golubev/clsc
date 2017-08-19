daa24af008cdbb0639aeaa494dbdf6f74d3b77e5
andrgolubev <andpgolubev@gmail.com>
2017-08-20 02:26:29 +0300

    memory_concepts.hpp: Added stack allocator Updated tests

--- core/memory_concepts.hpp
+++ core/memory_concepts.hpp
@@ -4,39 +4,48 @@
 #include <cassert>
 #include <cstdint>
 #include <cstdlib>
+#include <cmath>
 
 #include <vector>
 #include <algorithm>
 
 namespace clsc
 {
-    using block = void*;
+    using size_type = std::size_t; // TODO: use 64 bit ints instead of size_t ?
+    struct block
+    {
+        void* data;
+        size_type size;
+        bool operator==(const block& other) const
+        {
+            return this->size == other.size && this->data == other.data;
+        }
+    };
 
     class null_allocator
     {
     public:
-        block allocate(std::size_t) { return nullptr; }
-        void deallocate(block blk) { assert(blk == block(nullptr)); }
-        bool owns(block blk) { return blk == block(nullptr); }
+        block allocate(size_type) { return {nullptr, 0}; }
+        void deallocate(block& blk) { assert(blk.data == nullptr); }
+        bool owns(const block& blk) { return blk.data == nullptr; }
     };
 
-    // TODO: support basic malloc allocator
     class malloc_allocator
     {
     public:
-        block allocate(std::size_t size)
+        block allocate(size_type size)
         {
-            block blk = block(std::malloc(size));
+            block blk = {std::malloc(size), size};
             m_owned_data.push_back(blk);
             return blk;
         }
-        void deallocate(block blk)
+        void deallocate(block& blk)
         {
-            free(blk);
-            blk = block(nullptr);
+            free(blk.data);
+            blk = {nullptr, 0};
         }
         // temporary to have owns here
-        bool owns(block blk)
+        bool owns(const block& blk)
         {
             return std::find(m_owned_data.cbegin(), m_owned_data.cend(), blk) != m_owned_data.cend();
         }
@@ -48,16 +57,16 @@ namespace clsc
     class fallback_allocator : private PrimaryAllocator, private FallbackAllocator
     {
     public:
-        block allocate(std::size_t size)
+        block allocate(size_type size)
         {
             block blk = PrimaryAllocator::allocate(size);
-            if (!blk) // !blk.ptr
+            if (blk.data == nullptr)
             {
                 blk = FallbackAllocator::allocate(size);
             }
             return blk;
         }
-        void deallocate(block blk)
+        void deallocate(block& blk)
         {
             if (PrimaryAllocator::owns(blk)) // primary has to have ::owns() method
             {
@@ -68,11 +77,65 @@ namespace clsc
                 FallbackAllocator::deallocate(blk);
             }
         }
-        bool owns(block blk)
+        bool owns(const block& blk)
         {
             return PrimaryAllocator::owns(blk) || FallbackAllocator::owns(blk);
         }
     };
+
+    namespace __private
+    {
+        constexpr static size_type __default_stack_alloc_size = 8192;
+    }
+    template<size_type n>
+    class stack_allocator
+    {
+        using single_byte = char;
+        single_byte m_storage[n];
+        single_byte* m_pos;
+
+        int align(size_type s)
+        {
+            // bad cast and +1 - to not have issues when log2(s) returns 0
+            auto power = uint64_t(std::log2(s));
+            return std::pow(2, power + 1);
+        }
+    public:
+        stack_allocator() : m_pos(m_storage) {}
+        block allocate(size_type s)
+        {
+            const auto s_after_aligned = align(s);
+            if (s_after_aligned > (m_storage + n) - m_pos)
+            {
+                return {nullptr, 0};
+            }
+            block blk = {m_pos, s};
+            m_pos += s_after_aligned;
+            return blk;
+        }
+        void deallocate(block& blk)
+        {
+            // only deallocate from the last position
+            if ((static_cast<single_byte*>(blk.data) + align(blk.size)) == m_pos)
+            {
+                m_pos = static_cast<single_byte*>(blk.data);
+                blk = {nullptr, 0};
+            }
+        }
+        bool owns(const block& blk)
+        {
+            return blk.data >= m_storage && blk.data < m_storage + n;
+        }
+        void deallocate_all()
+        {
+            m_pos = m_storage;
+        }
+    };
+
+
+    // defines:
+    using default_allocator = fallback_allocator<malloc_allocator, null_allocator>;
+    using simple_allocator = fallback_allocator<stack_allocator<__private::__default_stack_alloc_size>, malloc_allocator>;
 }
 
 #endif
--- tests/memory_concepts.cpp
+++ tests/memory_concepts.cpp
@@ -2,10 +2,49 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+namespace
+{
+    constexpr std::size_t PLAIN_SIZE = 1;
+
+    template<typename T>
+    struct SIZE_ON_TYPE
+    {
+        constexpr static std::size_t value = PLAIN_SIZE * sizeof(T);
+    };
+
+    template<typename T>
+    auto SIZE = SIZE_ON_TYPE<T>::value;
+}
+
 TEST(memory_concepts_tests, fallback_allocator)
 {
     clsc::fallback_allocator<clsc::malloc_allocator, clsc::null_allocator> allocator;
-    int* my_ptr = (int*)allocator.allocate(1);
+    auto my_ptr = allocator.allocate(SIZE<int>);
+    EXPECT_NE(my_ptr.data, nullptr);
+    EXPECT_EQ(my_ptr.size, SIZE<int>);
     allocator.deallocate(my_ptr);
-    std::cout << my_ptr;
+    EXPECT_EQ(my_ptr.data, nullptr);
+    EXPECT_EQ(my_ptr.size, 0);
+}
+
+TEST(memory_concepts_tests, default_allocator)
+{
+    clsc::default_allocator allocator;
+    auto my_ptr = allocator.allocate(SIZE<int>);
+    EXPECT_NE(my_ptr.data, nullptr);
+    EXPECT_EQ(my_ptr.size, SIZE<int>);
+    allocator.deallocate(my_ptr);
+    EXPECT_EQ(my_ptr.data, nullptr);
+    EXPECT_EQ(my_ptr.size, 0);
+}
+
+TEST(memory_concepts_tests, simple_allocator)
+{
+    clsc::simple_allocator allocator;
+    auto my_ptr = allocator.allocate(SIZE<int>);
+    EXPECT_NE(my_ptr.data, nullptr);
+    EXPECT_EQ(my_ptr.size, SIZE<int>);
+    allocator.deallocate(my_ptr);
+    EXPECT_EQ(my_ptr.data, nullptr);
+    EXPECT_EQ(my_ptr.size, 0);
 }
