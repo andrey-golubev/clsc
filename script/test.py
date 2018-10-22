693e017203e66ef50dad14726118a0f90ed80746
andrgolubev <andpgolubev@gmail.com>
2018-10-22 22:42:43 +0300

    Fix function name in test.py

--- script/test.py
+++ script/test.py
@@ -28,7 +28,7 @@ def run_steps(steps, work_dir='.'):
         sys.stdout.flush()
 
 
-def build(debug, raw_args, work_dir='.'):
+def test(debug, raw_args, work_dir='.'):
     t = 'DEBUG' if debug else 'RELEASE'
     build_type = '-DCMAKE_BUILD_TYPE={t}'.format(t=t)
     steps = """
@@ -46,7 +46,7 @@ def main():
     if os.path.exists(workdir):
         shutil.rmtree(workdir)
         os.makedirs(workdir)
-    build(args.debug, args.raw_cmake, workdir)
+    test(args.debug, args.raw_cmake, workdir)
     return 0
 
 
